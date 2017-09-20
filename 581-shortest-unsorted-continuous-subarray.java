2b9fbda9f9eb9af7ea1e6cdf902c59f50fb33f94
xiaoydin <xiaoydin@cisco.com>
2017-09-21 00:04:16 +0800

    keep

--- 581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray.java
@@ -10,11 +10,20 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 class Solution {
 	public int findUnsortedSubarray(int[] nums) {
-		int a = 0, b = 0;
+		int a = -1, b = -1;
 		int min = nums[nums.length - 1], max = nums[0];
-		for(int i = 0; i < nums.length; i++){
+		for(int i = 1; i < nums.length; i++){
 			max = Math.max(max, nums[i]);
+			if(nums[i] < max) {
+				b = i;
+			}
+			min = Math.min(min, nums[nums.length - i - 1]);
+			if(nums[nums.length - i - 1] > min){
+				a =  nums.length - i - 1;
+			}
 		}
+		if(a == -1) return 0;
+		return b - a + 1;
     }
 	
 	public static void main(String args[]){
